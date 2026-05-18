84f9727e516c62674927f022bd54d0034c552b00
VinzentRisch <risch.vinzent@gmail.com>
2026-05-18 16:21:06 +0200

    chnaged the feature table creation to work with new header

--- q2_amrfinderplus/feature_table.py
+++ q2_amrfinderplus/feature_table.py
@@ -11,7 +11,6 @@ def create_feature_table(
     sample_dict = annotations.annotation_dict()
 
     level_mapping = {
-        "gene": "Gene symbol",
         "class": "Class",
         "subclass": "Subclass",
     }
@@ -24,12 +23,17 @@ def create_feature_table(
     for sample_id, file_dict in sample_dict.items():
         for _id, file_fp in file_dict.items():
             try:
+                if level == "gene":
+                    level_column = _get_gene_symbol_column(file_fp)
+                else:
+                    level_column = level_mapping[level]
+
                 file_df = pd.read_csv(
                     filepath_or_buffer=file_fp,
                     sep="\t",
                     usecols=[
                         "Contig id",
-                        level_mapping[level],
+                        level_column,
                         "Start",
                         "Stop",
                         "Strand",
@@ -51,6 +55,13 @@ def create_feature_table(
 
     # Drop duplicated rows and pivot table
     df.drop_duplicates(keep="first", inplace=True)
-    df_pivot = pd.crosstab(df["Contig id"], df[level_mapping[level]])
+    df_pivot = pd.crosstab(df["Contig id"], df[level_column])
 
     return df_pivot
+
+
+def _get_gene_symbol_column(file_fp):
+    header = pd.read_csv(file_fp, sep="\t", nrows=0).columns
+    if "Gene symbol" in header:
+        return "Gene symbol"
+    return "Element symbol"
--- q2_amrfinderplus/tests/test_feature_table.py
+++ q2_amrfinderplus/tests/test_feature_table.py
@@ -1,15 +1,16 @@
 import pandas as pd
 from qiime2.plugin.testing import TestPluginBase
 
-from q2_amrfinderplus.feature_table import create_feature_table
+from q2_amrfinderplus.feature_table import _get_gene_symbol_column, create_feature_table
 from q2_amrfinderplus.types import AMRFinderPlusAnnotationsDirFmt
 
 
 class TestFetchAMRFinderPlusDB(TestPluginBase):
     package = "q2_amrfinderplus.tests"
 
-    def test_create_feature_table_gene(self):
-        exp = pd.DataFrame(
+    def setUp(self):
+        super().setUp()
+        self.exp_gene_table = pd.DataFrame(
             {
                 "arsR": [0, 0, 0, 0, 1],
                 "blaOXA": [0, 0, 1, 0, 0],
@@ -20,13 +21,39 @@ class TestFetchAMRFinderPlusDB(TestPluginBase):
             },
             index=["contig01", "contig02", "contig03", "contig08", "contig13"],
         )
-        exp.index.name = "Contig id"
+        self.exp_gene_table.index.name = "Contig id"
+
+    def test_create_feature_table_gene(self):
+        exp = self.exp_gene_table.copy()
         exp.columns.name = "Gene symbol"
         annotations = AMRFinderPlusAnnotationsDirFmt(
             self.get_data_path("annotations_contigs_1"), mode="r"
         )
         obs = create_feature_table(annotations, level="gene")
         pd.testing.assert_frame_equal(exp, obs)
+
+    def test_create_feature_table_gene_new_header(self):
+        exp = self.exp_gene_table.copy()
+        exp.columns.name = "Element symbol"
+        annotations = AMRFinderPlusAnnotationsDirFmt(
+            self.get_data_path("annotations_contigs_new_header"), mode="r"
+        )
+        obs = create_feature_table(annotations, level="gene")
+        pd.testing.assert_frame_equal(exp, obs)
+
+    def test_get_gene_symbol_column_old_header(self):
+        filepath = self.get_data_path(
+            "annotations_contigs_1/sample1_amr_annotations.tsv"
+        )
+        obs = _get_gene_symbol_column(filepath)
+        self.assertEqual(obs, "Gene symbol")
+
+    def test_get_gene_symbol_column_new_header(self):
+        filepath = self.get_data_path(
+            "annotations_contigs_new_header/sample1_amr_annotations.tsv"
+        )
+        obs = _get_gene_symbol_column(filepath)
+        self.assertEqual(obs, "Element symbol")
 
     def test_create_feature_table_class(self):
         exp = pd.DataFrame(
